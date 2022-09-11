40bc965bcb12f0820317dd80602226798e08bae7
harry thomas <harryt1905@gmail.com>
2022-09-10 21:13:48 -0400

    unFORgettable

--- chorus-while.js
+++ chorus-while.js
@@ -17,3 +17,9 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
+
+const chorus3 = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
